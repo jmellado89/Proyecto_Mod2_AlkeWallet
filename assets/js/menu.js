b21602065af6b1efadc494ad4b762acd55ff9cef
Jime Mellado <mellado.jimena@gmail.com>
2026-01-11 01:01:41 -0300

    Menu actualizado con Boostrap y JS

---
+++ assets/js/menu.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+  $("#btnDeposit").click(function () {
+    const alerta = `
+                    <div class="alert alert-primary" role="alert">
+            Redirigiendo a pantalla de Depositar dinero...
+        </div>`;
+
+    $("#contenedor-alertas").html(alerta);
+
+    setTimeout(function () {
+      window.location.href = "deposit.html";
+    }, 2000);
+  });
+
+  $("#btnSendMoney").click(function () {
+    const alerta = `
+                    <div class="alert alert-primary" role="alert">
+            Redirigiendo a pantalla de Enviar dinero...
+        </div>`;
+
+    $("#contenedor-alertas").html(alerta);
+
+    setTimeout(function () {
+      window.location.href = "sendmoney.html";
+    }, 2000);
+  });
+
+  $("#btnTransactions").click(function () {
+    const alerta = `
+                    <div class="alert alert-primary" role="alert">
+            Redirigiendo a pantalla de Mis Movimientos...
+        </div>`;
+
+    $("#contenedor-alertas").html(alerta);
+
+    setTimeout(function () {
+      window.location.href = "transactions.html";
+    }, 2000);
+  });
+
+  $("#endSession").click(function () {
+    const alerta = `
+                    <div class="alert alert-primary" role="alert">
+            Cerrando sesión...
+        </div>`;
+
+    $("#contenedor-alertas").html(alerta);
+
+    setTimeout(function () {
+      window.location.href = "indexv2.html";
+    }, 2000);
+  });
+});
