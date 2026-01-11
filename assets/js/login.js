f799702853522c9ce57728783edbbb76458230ce
Jime Mellado <mellado.jimena@gmail.com>
2026-01-11 19:20:02 -0300

    Se añade modal de registro a pantalla de Login

--- assets/js/login.js
+++ assets/js/login.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 
     if (inputEmail == "test@test.com" && inputPassword == "test") {
       const alerta = `
-                    <div class="alert alert-primary" role="alert">
+                    <div class="alert alert-primary shadow fixed-top m-3" role="alert">
             Bienvenido a tu Alke Wallet!
         </div>`;
 
@@ -20,9 +20,18 @@ $(document).ready(function () {
     }
   });
 
-  $("#btn-register").on("click", function () {
-    window.location.href = "register.html";
-  });
+  // $("#btn-register").on("click", function () {
+  //   const alerta = `
+  //                   <div class="alert alert-primary shadow fixed-top m-3" role="alert">
+  //           Redirigiendo a formulario de Registro...
+  //       </div>`;
+
+  //   $("#contenedor-alertas").html(alerta);
+
+  //   setTimeout(function () {
+  //     window.location.href = "register.html";
+  //   }, 2000);
+  // });
 
   $("#btn-inicio").on("click", function () {
     window.location.href = "indexv2.html";
